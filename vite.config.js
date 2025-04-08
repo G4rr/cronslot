76ce44c38e2c8e08b49e6540414a6fc7a99446d5
Oleksii Pryshchepa <73528295+G4rr@users.noreply.github.com>
2025-04-08 10:34:56 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./" // important for GitHub Pages/Vercel subpath handling
+  plugins: [react()]
 })
